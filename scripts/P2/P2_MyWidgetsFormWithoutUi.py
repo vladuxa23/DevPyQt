96f48f6777689f0f40edf8a8f32a28324cad9f84
vladuxa23 <vlad_1995.02@mail.ru>
2022-06-17 19:09:18 +0300

    2022_Q2 after Practice_2 group_2

--- scripts/P2/P2_MyWidgetsFormWithoutUi.py
+++ scripts/P2/P2_MyWidgetsFormWithoutUi.py
@@ -1,4 +1,4 @@
-from PySide2 import QtWidgets, QtCore
+from PySide2 import QtWidgets, QtCore, QtGui
 
 
 class MyWindow(QtWidgets.QWidget):
@@ -9,7 +9,6 @@ class MyWindow(QtWidgets.QWidget):
         self.initUi()
 
     def initUi(self):
-
         self.pb_left_top = QtWidgets.QPushButton()
         self.pb_left_top.setText("Лево/верх")
         self.pb_left_top.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,
@@ -18,22 +17,22 @@ class MyWindow(QtWidgets.QWidget):
         self.pb_right_top = QtWidgets.QPushButton()
         self.pb_right_top.setText("Право/верх")
         self.pb_right_top.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,
-                                       QtWidgets.QSizePolicy.Policy.Expanding)
+                                        QtWidgets.QSizePolicy.Policy.Expanding)
 
         self.pb_center = QtWidgets.QPushButton()
         self.pb_center.setText("Центр")
         self.pb_center.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,
-                                       QtWidgets.QSizePolicy.Policy.Expanding)
+                                     QtWidgets.QSizePolicy.Policy.Expanding)
 
         self.pb_left_bottom = QtWidgets.QPushButton()
         self.pb_left_bottom.setText("Лево/низ")
         self.pb_left_bottom.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,
-                                       QtWidgets.QSizePolicy.Policy.Expanding)
+                                          QtWidgets.QSizePolicy.Policy.Expanding)
 
         self.pb_right_bottom = QtWidgets.QPushButton()
         self.pb_right_bottom.setText("Право/низ")
         self.pb_right_bottom.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,
-                                       QtWidgets.QSizePolicy.Policy.Expanding)
+                                           QtWidgets.QSizePolicy.Policy.Expanding)
 
         self.pb_get_parameters = QtWidgets.QPushButton()
         self.pb_get_parameters.setText("Получить параметры")
@@ -48,7 +47,7 @@ class MyWindow(QtWidgets.QWidget):
         self.slider = QtWidgets.QSlider()
         self.slider.setOrientation(QtCore.Qt.Orientation.Horizontal)
 
-        self.textEdit = QtWidgets.QPlainTextEdit()
+        self.plainTextEdit = QtWidgets.QPlainTextEdit()
 
         # LAYOUTS
 
@@ -107,10 +106,79 @@ class MyWindow(QtWidgets.QWidget):
         main_layout = QtWidgets.QHBoxLayout()
 
         main_layout.addLayout(main_layout_left)
-        main_layout.addWidget(self.textEdit)
+        main_layout.addWidget(self.plainTextEdit)
         # main_layout
 
         self.setLayout(main_layout)
+
+        # SIGNALS
+        self.pb_left_top.clicked.connect(self.onPBLTClicked)
+        self.pb_right_top.clicked.connect(self.onPBRTclicked)
+        self.pb_left_bottom.clicked.connect(self.onPBLBClicked)
+        self.pb_right_bottom.clicked.connect(self.onPBRBClicked)
+        self.pb_center.clicked.connect(self.onPBCClicked)
+        self.pb_get_parameters.clicked.connect(self.getScreenInfo)
+
+    def onPBLTClicked(self):
+        self.move(0, 0)
+
+    def onPBRTclicked(self):
+        screen_width = QtWidgets.QApplication.screenAt(self.pos()).size().width()
+        program_width = self.width()
+
+        self.move(screen_width - program_width, 0)
+
+    def onPBLBClicked(self):
+        screen_height = QtWidgets.QApplication.screenAt(self.pos()).size().height()
+        program_height = self.height()
+
+        self.move(0, screen_height - program_height - 100)
+
+    def onPBRBClicked(self):
+        screen_width = QtWidgets.QApplication.screenAt(self.pos()).size().width()
+        program_width = self.width()
+
+        screen_height = QtWidgets.QApplication.screenAt(self.pos()).size().height()
+        program_height = self.height()
+
+        self.move(screen_width - program_width, screen_height - program_height - 100)
+
+    def onPBCClicked(self):
+        screen_width = QtWidgets.QApplication.screenAt(self.pos()).size().width()
+        program_width = self.width()
+
+        screen_height = QtWidgets.QApplication.screenAt(self.pos()).size().height()
+        program_height = self.height()
+
+        self.move(screen_width / 2 - program_width / 2,
+                  screen_height / 2 - program_height / 2)
+
+    def getScreenInfo(self):
+        print(QtWidgets.QApplication.screens())
+        print(QtWidgets.QApplication.primaryScreen())
+        self.plainTextEdit.appendPlainText(str(QtWidgets.QApplication.primaryScreen()))
+
+        print(self.size())
+
+        print(self.pos())
+
+    def changeEvent(self, event:QtCore.QEvent) -> None:
+        # print(event.type())
+        if event.type() == QtCore.QEvent.Type.WindowStateChange:
+            if self.isMinimized():
+                print("Окно свёрнуто")
+
+    def moveEvent(self, event:QtGui.QMoveEvent) -> None:
+        print(self.pos())
+
+    def resizeEvent(self, event:QtGui.QResizeEvent) -> None:
+        print("ok")
+
+    def event(self, event:QtCore.QEvent) -> bool:
+        if event.type() == QtCore.QEvent.Resize:
+            print(self.size())
+
+        return QtWidgets.QWidget.event(self, event)
 
 
 if __name__ == '__main__':
@@ -120,4 +188,3 @@ if __name__ == '__main__':
     win.show()
 
     app.exec_()
-
