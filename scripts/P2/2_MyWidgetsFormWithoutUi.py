376ee4dcad66dc68e6cf8f9477ca80948b88f56a
vlad <vlad_1995.02@mail.ru>
2022-06-18 13:53:54 +0300

    after practice 2021/1 + 2021/3

--- scripts/P2/2_MyWidgetsFormWithoutUi.py
+++ scripts/P2/2_MyWidgetsFormWithoutUi.py
@@ -1,3 +1,5 @@
+import time
+
 from PySide2 import QtWidgets, QtCore, QtGui
 
 
@@ -111,74 +113,147 @@ class MyWindow(QtWidgets.QWidget):
 
         self.setLayout(main_layout)
 
-        # SIGNALS
-        self.pb_left_top.clicked.connect(self.onPBLTClicked)
-        self.pb_right_top.clicked.connect(self.onPBRTclicked)
-        self.pb_left_bottom.clicked.connect(self.onPBLBClicked)
-        self.pb_right_bottom.clicked.connect(self.onPBRBClicked)
-        self.pb_center.clicked.connect(self.onPBCClicked)
-        self.pb_get_parameters.clicked.connect(self.getScreenInfo)
+    # ГРУППА 2021-9/1 + 2021-9/3
+    #     self.pb_left_top.clicked.connect(self.onPBLTClicked)
+    #     self.pb_center.clicked.connect(self.onPBCClicked)
+    #     self.pb_right_top.clicked.connect(self.onPBRTClicked)
+    #
+    #     self.pb_get_parameters.clicked.connect(self.onPBGetParameters)
+    #
+    # def onPBLTClicked(self):
+    #     self.move(0, 0)
+    #
+    # def onPBCClicked(self):
+    #     screen_width = QtWidgets.QApplication.primaryScreen().size().width()
+    #     screen_height = QtWidgets.QApplication.primaryScreen().size().height()
+    #
+    #     pr_width = self.size().width()
+    #     pr_height = self.size().height()
+    #
+    #     self.move(
+    #         screen_width / 2 - pr_width / 2,
+    #         screen_height / 2 - pr_height / 2
+    #     )
+    #
+    # def onPBRTClicked(self):
+    #     desktop = QtWidgets.QDesktopWidget()
+    #     print(desktop.size())
+    #     print(desktop.screenGeometry().y())
+    #     print(desktop.availableGeometry())
+    #
+    #     print(QtWidgets.QApplication.primaryScreen().availableGeometry())
+    #
+    #
+    # def onPBLBClicked(self):
+    #     pass
+    #
+    # def onPBRBClicked(self):
+    #     pass
+    #
+    # def onPBGetParameters(self):
+    #     print(QtWidgets.QApplication.screens())
+    #
+    #     print(QtWidgets.QApplication.primaryScreen().name())
+    #
+    #     self.plainTextEdit.appendPlainText(QtWidgets.QApplication.primaryScreen().name())
+    #
+    #     print(QtWidgets.QApplication.screenAt(self.pos()))
+    #
+    #     print(self.minimumSize().width())
+    #     print(self.minimumSize().height())
+    #
+    #     print(self.pos())
 
-    def onPBLTClicked(self):
-        self.move(0, 0)
+    # def changeEvent(self, event:QtCore.QEvent) -> None:
+    #     if event.type() == QtCore.QEvent.WindowStateChange:
+    #         if self.isMinimized():
+    #             self.plainTextEdit.appendPlainText(f"{time.ctime()}: Окно свёрнуто")
+    #
+    #         if self.isMaximized():
+    #             self.plainTextEdit.appendPlainText(f"{time.ctime()}: Окно развёрнуто")
+    #
+    #         if self.isHidden():
+    #             self.plainTextEdit.appendPlainText(f"{time.ctime()}: Окно спрятано")
+    #
+    # def moveEvent(self, event:QtGui.QMoveEvent) -> None:
+    #     print(self.pos().x(), self.pos().y())
+    #
+    # def resizeEvent(self, event:QtGui.QResizeEvent) -> None:
+    #     print(self.size())
+    #
+    # def keyPressEvent(self, event:QtGui.QKeyEvent) -> None:
+    #     print(event.key())
 
-    def onPBRTclicked(self):
-        screen_width = QtWidgets.QApplication.screenAt(self.pos()).size().width()
-        program_width = self.width()
 
-        self.move(screen_width - program_width, 0)
-
-    def onPBLBClicked(self):
-        screen_height = QtWidgets.QApplication.screenAt(self.pos()).size().height()
-        program_height = self.height()
-
-        self.move(0, screen_height - program_height - 100)
-
-    def onPBRBClicked(self):
-        screen_width = QtWidgets.QApplication.screenAt(self.pos()).size().width()
-        program_width = self.width()
-
-        screen_height = QtWidgets.QApplication.screenAt(self.pos()).size().height()
-        program_height = self.height()
-
-        self.move(screen_width - program_width, screen_height - program_height - 100)
-
-    def onPBCClicked(self):
-        screen_width = QtWidgets.QApplication.screenAt(self.pos()).size().width()
-        program_width = self.width()
-
-        screen_height = QtWidgets.QApplication.screenAt(self.pos()).size().height()
-        program_height = self.height()
-
-        self.move(screen_width / 2 - program_width / 2,
-                  screen_height / 2 - program_height / 2)
-
-    def getScreenInfo(self):
-        print(QtWidgets.QApplication.screens())
-        print(QtWidgets.QApplication.primaryScreen())
-        self.plainTextEdit.appendPlainText(str(QtWidgets.QApplication.primaryScreen()))
-
-        print(self.size())
-
-        print(self.pos())
-
-    def changeEvent(self, event:QtCore.QEvent) -> None:
-        # print(event.type())
-        if event.type() == QtCore.QEvent.Type.WindowStateChange:
-            if self.isMinimized():
-                print("Окно свёрнуто")
-
-    def moveEvent(self, event:QtGui.QMoveEvent) -> None:
-        print(self.pos())
-
-    def resizeEvent(self, event:QtGui.QResizeEvent) -> None:
-        print("ok")
-
-    def event(self, event:QtCore.QEvent) -> bool:
-        if event.type() == QtCore.QEvent.Resize:
-            print(self.size())
-
-        return QtWidgets.QWidget.event(self, event)
+    # Группа 2021-1
+    #     # SIGNALS
+    #     self.pb_left_top.clicked.connect(self.onPBLTClicked)
+    #     self.pb_right_top.clicked.connect(self.onPBRTclicked)
+    #     self.pb_left_bottom.clicked.connect(self.onPBLBClicked)
+    #     self.pb_right_bottom.clicked.connect(self.onPBRBClicked)
+    #     self.pb_center.clicked.connect(self.onPBCClicked)
+    #     self.pb_get_parameters.clicked.connect(self.getScreenInfo)
+    #
+    # def onPBLTClicked(self):
+    #     self.move(0, 0)
+    #
+    # def onPBRTclicked(self):
+    #     screen_width = QtWidgets.QApplication.screenAt(self.pos()).size().width()
+    #     program_width = self.width()
+    #
+    #     self.move(screen_width - program_width, 0)
+    #
+    # def onPBLBClicked(self):
+    #     screen_height = QtWidgets.QApplication.screenAt(self.pos()).size().height()
+    #     program_height = self.height()
+    #
+    #     self.move(0, screen_height - program_height - 100)
+    #
+    # def onPBRBClicked(self):
+    #     screen_width = QtWidgets.QApplication.screenAt(self.pos()).size().width()
+    #     program_width = self.width()
+    #
+    #     screen_height = QtWidgets.QApplication.screenAt(self.pos()).size().height()
+    #     program_height = self.height()
+    #
+    #     self.move(screen_width - program_width, screen_height - program_height - 100)
+    #
+    # def onPBCClicked(self):
+    #     screen_width = QtWidgets.QApplication.screenAt(self.pos()).size().width()
+    #     program_width = self.width()
+    #
+    #     screen_height = QtWidgets.QApplication.screenAt(self.pos()).size().height()
+    #     program_height = self.height()
+    #
+    #     self.move(screen_width / 2 - program_width / 2,
+    #               screen_height / 2 - program_height / 2)
+    #
+    # def getScreenInfo(self):
+    #     print(QtWidgets.QApplication.screens())
+    #     print(QtWidgets.QApplication.primaryScreen())
+    #     self.plainTextEdit.appendPlainText(str(QtWidgets.QApplication.primaryScreen()))
+    #
+    #     print(self.size())
+    #
+    #     print(self.pos())
+    #
+    # def changeEvent(self, event:QtCore.QEvent) -> None:
+    #     # print(event.type())
+    #     if event.type() == QtCore.QEvent.Type.WindowStateChange:
+    #         if self.isMinimized():
+    #             print("Окно свёрнуто")
+    #
+    # def moveEvent(self, event:QtGui.QMoveEvent) -> None:
+    #     print(self.pos())
+    #
+    # def resizeEvent(self, event:QtGui.QResizeEvent) -> None:
+    #     print("ok")
+    #
+    # def event(self, event:QtCore.QEvent) -> bool:
+    #     if event.type() == QtCore.QEvent.Resize:
+    #         print(self.size())
+    #
+    #     return QtWidgets.QWidget.event(self, event)
 
 
 if __name__ == '__main__':
