bb517446afae910b3d0adddb798a116d4e92eaf8
vladuxa23 <vlad_1995.02@mail.ru>
2022-06-23 20:09:32 +0300

    2022_Q2 before Practice 3 group 2

--- scripts/P3/0_Lection_Repeat.py
+++ scripts/P3/0_Lection_Repeat.py
@@ -68,26 +68,37 @@ class MyApp(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        # init treads
         self.initThreads()
+
+        # init ui
         self.initUi()
 
     def initUi(self):
-        # init ui
+        """
+        Метод инициализации пользовательского интерфейса
+        """
+
+        # init QLineEdit
         self.lineEdit = QtWidgets.QLineEdit()
         self.lineEdit.setPlaceholderText("Введите количество секунд")
 
+        # init QPushButton START
         self.pbStart = QtWidgets.QPushButton()
         self.pbStart.setText("Старт")
 
+        # init QPushButton STOP
         self.pbStop = QtWidgets.QPushButton()
         self.pbStop.setText("Стоп")
         self.pbStop.setEnabled(False)
 
+        # init main layout
         main_layout = QtWidgets.QVBoxLayout()
         main_layout.addWidget(self.lineEdit)
         main_layout.addWidget(self.pbStart)
         main_layout.addWidget(self.pbStop)
 
+        # set main layout on main widget
         self.setLayout(main_layout)
 
         # init signals
@@ -95,31 +106,55 @@ class MyApp(QtWidgets.QWidget):
         self.pbStop.clicked.connect(self.onPBStopClicked)
 
     def initThreads(self):
+        """
+        Метод инициализации потоков
+        """
+
+        # create QThread instance
         self.timerThread = TimerThread()
 
         # init signals
-        self.timerThread.started.connect(self.timerThreadStarted)
-        self.timerThread.finished.connect(self.timerThreadFinished)
+        self.timerThread.started.connect(self.timerThreadStarted)  # выполняется, когда метод run запущен
+        self.timerThread.finished.connect(self.timerThreadFinished)  # выполняется, когда метод run закончен
 
-        self.timerThread.timerSignal.connect(self.timerSignalEmit)
+        self.timerThread.timerSignal.connect(self.timerSignalEmit)  # выполняется, когда вызывавется метод emit у
+        # сигнала timerSignal в потоке self.timerThread
 
     def onPBStartClicked(self):
+        """
+        Метод-слот для отработки сигнала clicked виджета self.pbStart
+
+        Запускает поток, в котором будет выполняться бэкенд
+        """
+
         try:
-            self.timerThread.timerCount = int(self.lineEdit.text())
-            self.timerThread.start()
-        except ValueError:
+            self.timerThread.timerCount = int(self.lineEdit.text())  # установка числа относительно которого начнётся отсчёт
+            self.timerThread.start()  # запуск потока
+        except ValueError:  # обработка сценария, если пользователь введёт не число
             self.lineEdit.setText("")
             QtWidgets.QMessageBox.warning(self, "Ошибка", "Введено неправильное значение")
 
     def onPBStopClicked(self):
+        """
+        Метод-слот для отработки сигнала clicked виджета self.pbStop
+        """
+
         self.timerThread.status = False
 
     def timerThreadStarted(self):
+        """
+        Метод-слот для отработки сигнала started потока self.timerThread
+        """
+
         self.pbStart.setEnabled(False)
         self.pbStop.setEnabled(True)
         self.lineEdit.setEnabled(False)
 
     def timerThreadFinished(self):
+        """
+        Метод-слот для отработки сигнала finished потока self.timerThread
+        """
+
         self.pbStart.setEnabled(True)
         self.pbStop.setEnabled(False)
         self.lineEdit.setEnabled(True)
@@ -127,32 +162,31 @@ class MyApp(QtWidgets.QWidget):
         self.lineEdit.setText("")
 
     def timerSignalEmit(self, emit_value):
+        """
+        Метод-слот для отработки сигнала timerSignal потока self.timerThread
+
+        Будет срабатывать, когда в потоке у сигнала self.timerSignal, будет вызываться метод emit()
+        П-р: self.timerSignal.emit(str(self.timerCount))
+        """
+
         self.lineEdit.setText(emit_value)
 
 
 class TimerThread(QtCore.QThread):
-    timerSignal = QtCore.Signal(str)
-    
+    timerSignal = QtCore.Signal(str)  # создаём кастомный сигнал для потока
+
     def __init__(self, parent=None):
         super(TimerThread, self).__init__(parent)
 
-        self.timerCount = None
-        self.status = True
+        self.timerCount = None  # атрибут, который будет содержать количество оставшихся секунд
+        self.status = True  # атрибут, который является флагом, для остановки потока
 
     def run(self):
         self.status = True
         while self.status:
             time.sleep(1)
             self.timerCount -= 1
-            self.timerSignal.emit(str(self.timerCount))
-
-        # for i in range(self.timerCount, 0, -1):
-        #     # print(i)
-        #     self.timerSignal.emit(str(i))
-        #     time.sleep(1)
-
-
-
+            self.timerSignal.emit(str(self.timerCount))  # отправка данных из потока
 
 
 if __name__ == '__main__':
