df69e1d0546983548d91817ea96ab5e2d1c69e89
vladuxa23 <vlad_1995.02@mail.ru>
2022-06-23 18:56:11 +0300

    2022_Q2 before Practice 3 group 2

--- scripts/P3/0_Lection_Repeat.py
+++ scripts/P3/0_Lection_Repeat.py
@@ -1,69 +1,118 @@
 # Группа 2021-2
+# import time
+#
+# from PySide2 import QtCore, QtWidgets
+#
+# class MyTimer(QtWidgets.QWidget):
+#     def __init__(self, parent=None):
+#         super(MyTimer, self).__init__(parent)
+#
+#         self.initUi()
+#
+#         self.timerTread = MyTimerThread()
+#         self.timerTread.timer_signal.connect(self.setTimerCount)
+#
+#         self.timerTread.started.connect(lambda: self.lineEditTimer.setEnabled(False))
+#         self.timerTread.finished.connect(self.timerFinished)
+#
+#     def initUi(self):
+#         self.lineEditTimer = QtWidgets.QLineEdit()
+#         self.lineEditTimer.setPlaceholderText("Введите количество секунд")
+#
+#         self.pbStartTimer = QtWidgets.QPushButton()
+#         self.pbStartTimer.setText("Старт")
+#         self.pbStartTimer.clicked.connect(self.onPBStartTimerClicked)
+#
+#         mainLayout = QtWidgets.QVBoxLayout()
+#
+#         mainLayout.addWidget(self.lineEditTimer)
+#         mainLayout.addWidget(self.pbStartTimer)
+#
+#         self.setLayout(mainLayout)
+#
+#     def onPBStartTimerClicked(self):
+#         self.timerTread.setCounter(int(self.lineEditTimer.text()))
+#         self.timerTread.start()
+#
+#     def setTimerCount(self, count):
+#         self.lineEditTimer.setText(count)
+#
+#     def timerFinished(self):
+#         self.lineEditTimer.setEnabled(True)
+#         self.lineEditTimer.setText("")
+#
+#
+# class MyTimerThread(QtCore.QThread):
+#     timer_signal = QtCore.Signal(str)
+#
+#     def __init__(self, parent=None):
+#         super(MyTimerThread, self).__init__(parent)
+#
+#         self.time_count = 0
+#
+#     def setCounter(self, time_count):
+#         self.time_count = time_count
+#
+#     def run(self):
+#         for i in range(self.time_count, 0, -1):
+#             self.timer_signal.emit(str(i))
+#             time.sleep(1)
+
+
+# Группа 2021-1
 import time
+from PySide2 import QtWidgets, QtCore
 
-from PySide2 import QtCore, QtWidgets
 
-class MyTimer(QtWidgets.QWidget):
+class MyApp(QtWidgets.QWidget):
     def __init__(self, parent=None):
-        super(MyTimer, self).__init__(parent)
+        super().__init__(parent)
 
+        self.initThreads()
         self.initUi()
 
-        self.timerTread = MyTimerThread()
-        self.timerTread.timer_signal.connect(self.setTimerCount)
-
-        self.timerTread.started.connect(lambda: self.lineEditTimer.setEnabled(False))
-        self.timerTread.finished.connect(self.timerFinished)
-
     def initUi(self):
-        self.lineEditTimer = QtWidgets.QLineEdit()
-        self.lineEditTimer.setPlaceholderText("Введите количество секунд")
+        # init ui
+        self.lineEdit = QtWidgets.QLineEdit()
+        self.lineEdit.setPlaceholderText("Введите количество секунд")
 
-        self.pbStartTimer = QtWidgets.QPushButton()
-        self.pbStartTimer.setText("Старт")
-        self.pbStartTimer.clicked.connect(self.onPBStartTimerClicked)
+        self.pbStart = QtWidgets.QPushButton()
+        self.pbStart.setText("Старт")
 
-        mainLayout = QtWidgets.QVBoxLayout()
+        self.pbStop = QtWidgets.QPushButton()
+        self.pbStop.setText("Стоп")
 
-        mainLayout.addWidget(self.lineEditTimer)
-        mainLayout.addWidget(self.pbStartTimer)
+        main_layout = QtWidgets.QVBoxLayout()
+        main_layout.addWidget(self.lineEdit)
+        main_layout.addWidget(self.pbStart)
+        main_layout.addWidget(self.pbStop)
 
-        self.setLayout(mainLayout)
+        self.setLayout(main_layout)
 
-    def onPBStartTimerClicked(self):
-        self.timerTread.setCounter(int(self.lineEditTimer.text()))
-        self.timerTread.start()
+        # init signals
+        self.pbStart.clicked.connect(self.onPBStartClicked)
 
-    def setTimerCount(self, count):
-        self.lineEditTimer.setText(count)
+    def initThreads(self):
+        self.timerThread = TimerThread()
 
-    def timerFinished(self):
-        self.lineEditTimer.setEnabled(True)
-        self.lineEditTimer.setText("")
+    def onPBStartClicked(self):
+        pass
 
 
-class MyTimerThread(QtCore.QThread):
-    timer_signal = QtCore.Signal(str)
-
-    def __init__(self, parent=None):
-        super(MyTimerThread, self).__init__(parent)
-
-        self.time_count = 0
-
-    def setCounter(self, time_count):
-        self.time_count = time_count
+class TimerThread(QtCore.QThread):
 
     def run(self):
-        for i in range(self.time_count, 0, -1):
-            self.timer_signal.emit(str(i))
+        for i in range(10, 0, -1):
+            print(i)
             time.sleep(1)
+
 
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication()
 
-    win = MyTimer()
+    win = MyApp()
     win.show()
 
     app.exec_()
