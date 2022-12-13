315e7e176bbd73c333e539388a59d25b7b7865d4
r-elen <elen.ryazanova@gmail.com>
2022-12-13 15:05:45 +0300

    d_settings - done

--- scripts/Практика 2/a_Повторение/d_settings.py
+++ scripts/Практика 2/a_Повторение/d_settings.py
@@ -8,13 +8,31 @@
 в него сохранённый текст
 """
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore, QtGui
 
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.initUi()
+
+    def initUi(self):
+        setting = QtCore.QSettings("Test")
+
+        self.plainTextEdit = QtWidgets.QPlainTextEdit()
+        self.plainTextEdit.setPlainText(setting.value("plainTextValue", ""))
+        self.plainTextEdit.setPlaceholderText("Введите текст...")
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.plainTextEdit)
+
+        self.setLayout(layout)
+
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        settings = QtCore.QSettings("Test")
+        settings.setValue("plainTextValue", self.plainTextEdit.toPlainText())
 
 
 if __name__ == "__main__":
